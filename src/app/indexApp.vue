85bc21ea39e663c38310d28883921eddb97bfbfc
lwm98 <821256519@qq.com>
2019-10-27 17:34:54 +0800

    主页搭建

--- src/app/indexApp.vue
+++ src/app/indexApp.vue
@@ -3,17 +3,37 @@
     <el-container>
       <el-main>
         <div class="topBox">
-        <div class="search" id="search">
+          <div class="mylove" id="mylove">
+            <el-card class="box-card">
+              <div slot="header" class="clearfix">
+                <span>我喜欢</span>
+                <el-button style="float: right; padding: 3px 0" type="text">更多</el-button>
+                <div class="mylovepull" @mouseenter="myloveshow" @mouseout="mylovehide"><i class="el-icon-star-off"></i></div>
+              </div>
+              <div v-for="o in 4" :key="o" class="text item">
+                {{'列表内容 ' + o }}
+              </div>
+            </el-card>
+          </div>
+          <div class="search" id="search">
           <div class="myinput">
-            <el-input
-              placeholder="请输入您想听的歌曲"
-              v-model="input"
-              autofocus="true"
-              clearable>
-            </el-input>
+            <el-popover
+              placement="bottom"
+              title="今日推荐："
+              width="200"
+              trigger="hover"
+              content="我的快乐时代-陈奕迅">
+              <el-input
+                slot="reference"
+                placeholder="请输入您想听的歌曲"
+                v-model="inputData"
+                autofocus="true"
+                clearable>
+              </el-input>
+            </el-popover>
           </div>
           <div class="mybutton">
-              <el-button type="primary" icon="el-icon-search" :loading="myButtonIsLoading" @click="search">搜索</el-button>
+              <el-button type="primary" icon="el-icon-search" :loading="myButtonIsLoading" @click="search" :disabled="canSearch">搜索</el-button>
           </div>
         </div>
         </div>
@@ -30,7 +50,6 @@
           <div class="right" v-show="show">
             <h3>评论：</h3>
             <div class="comment">
-              <comments></comments>
             </div>
           </div>
           </transition>
@@ -44,24 +63,32 @@
 <script>
   export default {
     name: 'loginApp',
-    components: { comments },
     data () {
       return {
-        input: '',
+        inputData: '',
         show: false,
         isplay: true,
+        canSearch: true,
         myButtonIsLoading: false
       }
     },
     watch: {
+      inputData: function (val) {
+        console.log(val)
+        if (val != null && val !== '') {
+          this.canSearch = false
+        } else {
+          this.canSearch = true
+        }
+      },
       isplay: function (val) {
         console.log('值为' + val)
         if (val) {
           console.log('正在播放 点击停止')
-          document.getElementById("circle").className = 'circle rota'
+          document.getElementById('circle').className = 'circle rota'
         } else {
           console.log('已停止播放 点击继续')
-          document.getElementById("circle").className = 'circle'
+          document.getElementById('circle').className = 'circle'
         }
       }
     },
@@ -91,6 +118,23 @@
       },
       play () {
         this.isplay = !this.isplay
+      },
+      myloveshow () {
+        var current = -374
+        var target = 0
+        var myloveshowclock = setInterval(function () {
+          var step = (target - current) / 15
+          step = step > 0 ? Math.ceil(step) : Math.floor(step)
+          current = current + step
+          console.log(current)
+          document.getElementById('mylove').style.left = current + 'px'
+          if (current == target) {
+            clearInterval(myloveshowclock)
+          }
+        }, 20)
+      },
+      mylovehide () {
+        // document.getElementById('mylove').style.left = -374 + 'px'
       }
     }
   }
@@ -107,7 +151,39 @@
     height: 226px;
     border: 1px solid #E9EEF3;
     margin: 0;
+    position: relative;
   }
+  .topBox .mylove{
+    position: absolute;
+    top: 0;
+    left: -374px;
+  }
+  /*这是我喜欢的卡片↓*/
+  .text {
+    font-size: 14px;
+  }
+  .item {
+    margin-bottom: 18px;
+  }
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+  .box-card {
+    width: 350px;
+  }
+  .mylovepull{
+    position: absolute;
+    top: 0;
+    right: -13px;
+    border: 1px solid;
+    background-color: white;
+  }
+  /*到这里结束↑*/
   .bottomBox{
     width: 100%;
     height: 420px;
@@ -118,8 +194,10 @@
   .bottomBox .left,.bottomBox .right{
     width: 658px;
     height: 400px;
-    border: 1px solid black;
+    border: 1px solid white;
     padding: 0 20px;
+    border-radius: 4px;
+    box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
   }
   .bottomBox .left{
     text-align: center;
