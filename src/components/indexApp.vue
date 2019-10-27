a78ce0c887452773d568dcada2afa5521d4e2f1a
lwm98 <821256519@qq.com>
2019-10-27 14:15:01 +0800

    主页

--- src/components/indexApp.vue
+++ src/components/indexApp.vue
@@ -1,7 +1,24 @@
 <template>
   <div>
     <el-container>
-      <el-main>Main
+      <el-main>
+        <div class="topBox">
+        <div class="search" id="search">
+          <div class="myinput">
+            <el-input
+              placeholder="请输入您想听的歌曲"
+              v-model="input"
+              autofocus="true"
+              clearable>
+            </el-input>
+          </div>
+          <div class="mybutton">
+              <el-button type="primary" icon="el-icon-search" :loading="myButtonIsLoading" @click="search">搜索</el-button>
+          </div>
+        </div>
+          <div class="left">aaaaa</div>
+          <div class="right">bbbbb</div>
+        </div>
         <a href="home.html">跳转到home</a>
       </el-main>
     </el-container>
@@ -10,7 +27,37 @@
 
 <script>
   export default {
-    name: 'loginApp'
+    name: 'loginApp',
+    data () {
+      return {
+        input: '',
+        myButtonIsLoading: false
+      }
+    },
+    methods: {
+      search () {
+        var target = 0
+        var current = 250
+        this.myButtonIsLoading = true
+        var myClock = setInterval(function () {
+          var step = (target - current) / 15
+          step = step > 0 ? Math.ceil(step) : Math.floor(step)
+          current = current + step
+          document.getElementById('search').style.marginTop = current + 'px'
+          if (current === 0) {
+            clearInterval(myClock)
+          }
+        }, 10)
+
+        console.log(this.input)
+
+        const h = this.$createElement
+        this.$notify({
+          title: '提示',
+          message: h('i', {style: 'color: teal'}, '歌曲正在加载中，请稍候')
+        })
+      }
+    }
   }
 </script>
 
@@ -18,8 +65,23 @@
   .el-main {
     background-color: #E9EEF3;
     color: #333;
-    text-align: center;
-    line-height: 472px;
     height: 722px;
+  }
+  .topBox{
+    width: 100%;
+    height: 600px;
+    border: 1px solid;
+    margin: 0;
+  }
+  .search{
+    width: 650px;
+    display: flex;
+    justify-content: space-between;
+    margin: 0 auto;
+    margin-top: 250px;
+  }
+  .search .myinput{
+    width: 500px;
+    margin: 0 auto;
   }
 </style>
